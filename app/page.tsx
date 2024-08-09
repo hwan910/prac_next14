6dca0f71c7499a52eb3de6c320a7f54312631d7e
sunghwan <psh@dannect.com>
2024-08-09 14:20:08 +0900

    form modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,42 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm dark:bg-gray-600">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
-              In transit
-            </span>
-            <span className="text-4xl font-semibold dark:text-white">
-              Coolblue
-            </span>
-          </div>
-          <div className="size-12 rounded-full bg-orange-400" />
-        </div>
-        <div className="my-2 flex items-center gap-2">
-          <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full transition hover:bg-green-500 hover:scale-125">
-            Today
-          </span>
-          <span className="dark:text-gray-100">9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 absolute rounded-full w-full h-2" />
-          <div className="bg-green-400 absolute rounded-full w-2/3 h-2" />
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-300 ">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
+    <main
+      className="bg-gray-100 h-screen flex items-center justify-center p-5
+      sm:bg-red-200
+      md:bg-green-200
+      lg:bg-blue-200
+      xl:bg-blue-300
+      2xl:bg-blue-400
+      "
+    >
+      <div
+        className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2
+      "
+      >
+        <input
+          className=" pl-5 bg-gray-100 w-full rounded-full h-10 outline-none ring ring-transparent
+          focus:ring-blue-400 focus:ring-offset-2 transition-shadow
+          placeholder:drop-shadow-xl
+          invalid:focus:bg-red-100
+          invalid:focus:ring-red-500
+          peer
+          "
+          type="email"
+          required
+          placeholder="Eamil address"
+        />
+        <span className="pl-5 hidden text-red-500 peer-invalid:block">
+          올바른 형식이 아닙니다.
+        </span>
+        <button
+          className=" bg-gradient-to-tr from-cyan-400 to-purple-400  text-white py-2 rounded-full md:px-8 
+
+        peer-invalid:from-slate-400
+        peer-invalid:to-red-400
+        "
+        >
+          Submit
+        </button>
       </div>
     </main>
   );
